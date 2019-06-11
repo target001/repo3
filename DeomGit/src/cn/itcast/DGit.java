1a0f82f031b0e4ddc18ba2e8ae939154a0009881
admin <admin@qq.com>
2019-06-11 15:46:20 +0800

    12313123

---
+++ DeomGit/src/cn/itcast/DGit.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+public class DGit {
+    public static void main(String[] args) {
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+    }
+}
